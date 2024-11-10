83213b86e3600fcc6b63d18e1ad23e8e0479ea65
Flukeza07x <mr.fluke2544@gmail.com>
2024-11-10 17:42:21 +0700

    add day3 demo app

--- demoapp/src/components/MenuComponent.vue
+++ demoapp/src/components/MenuComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <router-link  to="/" class="navbar-brand">gVue Bootstrap</router-link>
+    <router-link  to="/" class="navbar-brand">Vue Bootstrap</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
