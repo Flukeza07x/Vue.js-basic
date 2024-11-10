b965f41b0d0290f751b184349ce4626aaa815118
Flukeza07x <mr.fluke2544@gmail.com>
2024-11-10 18:13:40 +0700

    day 3'

--- demoapp/src/App.vue
+++ demoapp/src/App.vue
@@ -1,25 +1,23 @@
 <template>
-<!-- Menu -->
-<menu-component/>
-
+  <!-- MENU -->
+  <MenuComponent />
 
   <router-view/>
 
   <!-- Footer -->
-<footer-component/>
+  <FooterComponent />
 
 </template>
+
 <script>
 
-import MenuComponent from '@/components/MenuComponent.vue'
-import FooterComponent from '@/components/FooterComponent.vue'
+  import MenuComponent from '@/components/MenuComponent.vue'
+  import FooterComponent from '@/components/FooterComponent.vue'
 
-export default {
-
-  components: {MenuComponent,
-  FooterComponent}
-
-}
+  export default {
+    components: {
+      MenuComponent,
+      FooterComponent
+    }
+  }
 </script>
-
-
--- demoapp/src/views/HomeView.vue
+++ demoapp/src/views/HomeView.vue
@@ -1,5 +1,3 @@
 <template>
-  <h1>home Page</h1>
+  <h1>Home Page</h1>
 </template>
-
-
